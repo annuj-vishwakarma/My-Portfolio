ddaff14814b2de86f4664fc398e29cd6b9339be5
anuj <vishwakarmaanuj61@gmail.com>
2024-01-04 20:37:26 +0530

    updated

--- script.js
+++ script.js
@@ -13,9 +13,29 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     const moonIcon = document.getElementById('moon-icon');
     moonIcon.classList.toggle('ri-moon-fill');
     moonIcon.classList.toggle('ri-sun-fill');
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Display the warning message
+    showWarningMessage();
+
+    // Simulate loading time (10 seconds)
+    setTimeout(function() {
+        // Hide loading screen
+        document.getElementById("loading-screen").style.display = "none";
+
+        // Hide warning message
+        hideWarningMessage();
+
+        // Show main content
+        document.getElementById("main-content").style.display = "block";
+    }, 10000); // 10000 milliseconds = 10 seconds
+});
