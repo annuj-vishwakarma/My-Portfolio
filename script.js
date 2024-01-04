ef24a0021acc8dcd0b7a07cc51901b5176311ba5
anuj <vishwakarmaanuj61@gmail.com>
2024-01-04 20:17:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// function toggleDarkMode() {
+//     document.body.classList.toggle('dark-mode');
+// }
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate loading time (10 seconds)
+    setTimeout(function() {
+        // Hide loading screen
+        document.getElementById("loading-screen").style.display = "none";
+
+        // Show main content
+        document.getElementById("main-content").style.display = "block";
+    }, 10000); // 10000 milliseconds = 10 seconds
+});
+
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    const moonIcon = document.getElementById('moon-icon');
+    moonIcon.classList.toggle('ri-moon-fill');
+    moonIcon.classList.toggle('ri-sun-fill');
+}
